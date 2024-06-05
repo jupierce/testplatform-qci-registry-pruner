46e77ed744f6bca51272c6daf61444ab760720c2
Justin Pierce <jupierce@redhat.com>
2024-06-05 14:48:02 -0400

    Update to use quay.io API The dxf del_alias removes the image manifest along with the tag. We only want to remove the tag and leave the image in place. If no further tags point to the image, quay will garbage collect it.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-import dxf
-import os
+import pathlib
+import time
+
+import requests
 import re
-import yaml
 import argparse
-from typing import List, Dict, Set
+from typing import List, Dict, Set, Optional
 from datetime import datetime, timedelta
 
 sha_digest = str  # e.g. "19cb0d56000e91966025d08f345f751d90882f87aad2a6af7c4602b72225aacf"
@@ -14,121 +15,109 @@ digest_tag_match = re.compile(r"^(?P<year>\d\d\d\d)(?P<month>\d\d)(?P<day>\d\d)_
 references: Dict[sha_digest, List] = dict()
 
 
-def add_reference(digest: sha_digest, alias: str):
-    if digest not in references:
-        references[digest] = list()
-    references[digest].append(alias)
+def delete_tag(repository: str, tag: str, token: str):
+    """Delete a tag from a Quay.io repository"""
+    delete_url = f"https://quay.io/api/v1/repository/{repository}/tag/{tag}"
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    response = requests.delete(delete_url, headers=headers)
+    if response.status_code == 204:
+        print(f"Tag '{tag}' deleted successfully")
+    else:
+        print(f"Failed to delete tag '{tag}': {response.status_code} {response.text}")
 
 
-def build_sha_tag(dt: datetime, digest: sha_digest):
-    """
-    Given a datetime and digest (e.g. "19cb0d56000e91966025d08f345f751d90882f87aad2a6af7c4602b72225aacf"),
-    returns YYYYMMDD_sha256_<digest>
-    :param dt: The date for the tag.
-    :param digest: The image digest
-    :return: A sha256 tag using the QCI format.
-    """
-    date_prefix = dt.strftime("%Y%m%d")
-    return f'{date_prefix}_sha256_{digest}'
+def fetch_tags(repository: str, token: str, page: int = 1, like: Optional[str] = None):
+    """Fetch tags from the Quay.io repository with pagination"""
+    like_adder = ''
+    if like:
+        like_adder = f'&filter_tag_name=like:{like}'
+    tags_url = f"https://quay.io/api/v1/repository/{repository}/tag/?page={page}&limit=100&onlyActiveTags=true" + like_adder
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    response = requests.get(tags_url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        tags = data.get("tags", [])
+        has_more = data.get("has_additional", False)
+        return tags, has_more
+    else:
+        raise IOError(f"Failed to fetch tags: {response.status_code} {response.text}")
 
 
 if __name__ == '__main__':
+    start_time = datetime.now()
     parser = argparse.ArgumentParser(description="Process some optional arguments.")
 
-    # Add optional arguments that accept a filename
+    parser.add_argument('--token', type=str, help='quay.io oauth application token')
     parser.add_argument('--confirm', action='store_true', help='Actually delete and refresh tags')
 
     # Parse the arguments
     args = parser.parse_args()
+    if not args.token:
+        print('OAuth token is required')
+        exit(1)
+    token = args.token
     confirm = args.confirm
 
-    start_time = datetime.now()
+    # Fetch all tags with pagination
+    page = 1
+    has_more = True
 
-    sanity_match = digest_tag_match.match(build_sha_tag(start_time, '0000'))
-    if not sanity_match or int(sanity_match.group('year')) != start_time.year or int(sanity_match.group('month')) != start_time.month or int(sanity_match.group('day')) != start_time.day:
-        # Sanity check the tag builder.
-        raise IOError('Logic error. Building unmatchable tags.')
-
-    dxf_obj = dxf.DXF('quay.io', repo='openshift/ci')
-    dxf_obj.authenticate(username=os.getenv('DXF_USERNAME'), password=os.getenv('DXF_PASSWORD'), actions=['*'])
-    last_auth_time = datetime.now()
-    reauth_period = timedelta(minutes=20)
-
-    prune_target_tags: Set[str] = set()
-    pruned_tags: Set[str] = set()
+    prune_target_tags = set()
+    pruned_tags = set()
     tag_count = 0
-    refresh_count = 0
     mod_by = 5
+    while has_more:
+        retries = 3
+        while retries > 0:
+            try:
+                tags, has_more = fetch_tags('openshift/ci', token, page, like='_sha256_')
+                break
+            except Exception as e:
+                print(f'Error retrieving tags: {e}')
+                time.sleep(5)
+                retries -= 1
+                if retries == 0:
+                    raise
 
-    for image_tag in dxf_obj.list_aliases(iterate=True):
-        tag_count += 1
-        match = digest_tag_match.match(image_tag)
-        if match:
-            digest: sha_digest = match.group('digest')
-            year = int(match.group('year'))
-            month = int(match.group('month'))
-            day = int(match.group('day'))
-            digest_tag_date = datetime(year, month, day)
-            add_reference(digest, image_tag)
+        # Iterate through tags and delete those that match the pattern "%_sha256_%"
+        for tag in tags:
+            tag_count += 1
+            image_tag = tag['name']
 
-            date_difference = start_time - digest_tag_date
-            days_difference = date_difference.days
-            if days_difference > 5 and image_tag not in prune_target_tags:
-                prune_target_tags.add(image_tag)
-                # Ultimately, we only want images that were promoted to be
-                # removed after several days of no longer being referenced
-                # by a non-sha tag. To achieve this, the pruner logic
-                # should apply a refresher sha tag to every non-sha tagged
-                # image each day.
-                # Once an image has not been referenced for 5 days straight,
-                # the pruner will remove the last sha tag and the image
-                # will be garbage collected.
-                if confirm:
-                    try:
-                        dxf_obj.del_alias(image_tag)
-                        print(f'Removed {image_tag}')
-                        pruned_tags.add(image_tag)
-                    except Exception as e:
-                        print(f'Error while trying to delete tag {image_tag}: {e}')
-                else:
-                    print(f'Would have removed {image_tag}')
+            if tag_count % mod_by == 0:
+                mod_by = min(mod_by * 2, 1000)
+                print(f'{tag_count} tags have been checked')
 
-        else:
-            # This is a non-sha tag and must be preserved. HTTP HEAD on the alias
-            # to receive digest information.
-            digest: sha_digest = dxf_obj.head_manifest_and_response(image_tag)[0].split(':')[-1]  # 'sha256:daf7f3efba70c9a03045d364a5b08977df8b0e5d48a3e11d1021ec588592e59c' => 'daf7f3efba70c9a03045d364a5b08977df8b0e5d48a3e11d1021ec588592e59c'
-            add_reference(digest, image_tag)
-            # Every time we see such a tag, we retag it with a current datetime sha tag to keep it around for the next 5 days.
-            new_tag = build_sha_tag(start_time, digest)
-            refresh_count += 1
-            if confirm:
-                try:
-                    dxf_obj.set_alias(new_tag, dxf_obj.get_alias(image_tag))
-                    print(f'Refreshed tag for: {image_tag} with {new_tag}')
-                except Exception as e:
-                    print(f'ERROR applying reference tag {image_tag}')
-            else:
-                print(f'Would have refreshed tag for: {image_tag} with {new_tag}')
+            match = digest_tag_match.match(image_tag)
+            if match:
+                digest: sha_digest = match.group('digest')
+                year = int(match.group('year'))
+                month = int(match.group('month'))
+                day = int(match.group('day'))
+                digest_tag_date = datetime(year, month, day)
 
-        if tag_count % mod_by == 0:
-            mod_by = min(mod_by * 2, 1000)
-            print(f'{tag_count} tags have been processed')
+                date_difference = start_time - digest_tag_date
+                days_difference = date_difference.days
+                if days_difference > 5 and image_tag not in prune_target_tags:
+                    prune_target_tags.add(image_tag)
+                    if confirm:
+                        try:
+                            delete_tag('openshift/ci', tag=image_tag, token=token)
+                            print(f'Removed {image_tag}')
+                            pruned_tags.add(image_tag)
+                        except Exception as e:
+                            print(f'Error while trying to delete tag {image_tag}: {e}')
+                    else:
+                        print(f'Would have removed {image_tag}')
 
-        current_time = datetime.now()
-        elapsed_time = current_time - last_auth_time
-
-        if elapsed_time >= reauth_period:
-            # Our token will expire periodically. Occasionally reauthenticate.
-            dxf_obj.authenticate(username=os.getenv('DXF_USERNAME'), password=os.getenv('DXF_PASSWORD'), actions=['*'])
-            last_auth_time = datetime.now()
-
-    with open('qci_references.yaml', mode='w+') as output:
-        print('Discovery complete -- writing (pre-pruner) registry information to disk')
-        yaml.safe_dump(references, output)
+        page += 1
 
     finish_time = datetime.now()
     print(f'Duration: {finish_time - start_time}')
     print(f'Total tags scanned: {tag_count}')
-    print(f'Tags refreshed (if --confirm): {refresh_count}')
     print(f'Tags pruned (if --confirm): {len(prune_target_tags)}')
     print(f'Tags actually pruned: {len(pruned_tags)}')
